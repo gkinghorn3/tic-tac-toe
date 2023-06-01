8aee2b4f2d9fec354527ae0fad8431788c36f747
grant kinghorn <gkinghorn3@icloud.com>
2023-06-02 00:05:23 +0100

    form validation added

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,17 @@
+// config buttons
 const player1EditBtn = document.getElementById("edit-player-1-btn");
 const player2EditBtn = document.getElementById("edit-player-2-btn");
+const closeConfigOverlayBtn = document.getElementById("close-config-popup");
+
+// overlay and player config popup
 const configPopup = document.getElementById("config-overlay");
 const configPopupBackdrop = document.getElementById("backdrop");
-const closeConfigOverlayBtn = document.getElementById("close-config-popup");
+
+const formElement = document.querySelector("form");
+const errorOutputElement = document.getElementById("config-error");
 
 player1EditBtn.addEventListener("click", openPlayerConfig);
 player2EditBtn.addEventListener("click", openPlayerConfig);
 closeConfigOverlayBtn.addEventListener("click", closePlayerConfig);
+
+formElement.addEventListener("submit", savePlayerConfig);
--- scripts/config.js
+++ scripts/config.js
@@ -6,4 +6,19 @@ function openPlayerConfig() {
 function closePlayerConfig() {
   configPopup.style.display = "none";
   configPopupBackdrop.style.display = "none";
+  errorOutputElement.textContent = "";
+  formElement.firstElementChild.classList.remove("input-error");
+}
+
+function savePlayerConfig(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  // method of formData object to get the value of a given input
+  const enteredPlayerName = formData.get("playername").trim();
+
+  if (!enteredPlayerName) {
+    event.target.firstElementChild.classList.add("input-error");
+    errorOutputElement.textContent = "Please enter a valid name";
+    return;
+  }
 }
