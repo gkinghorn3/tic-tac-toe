15d35bdd56b14a331bcd3a8bddde7fbc0eecc335
Grant <gkinghorn3@icloud.com>
2023-06-19 11:45:59 +0100

    board logic for keeping track of selected fields changed

--- scripts/game.js
+++ scripts/game.js
@@ -1,27 +1,35 @@
 function startNewGame() {
-    if (players[0].name === '' || players[1].name === '') {
-        alert('pleaser enter a name for each player')
-        return;
-    }
-    activePlayerNameElement.textContent = players[activePlayer].name;
-    gameAreaElement.style.display = 'block';
+  if (players[0].name === "" || players[1].name === "") {
+    alert("pleaser enter a name for each player");
+    return;
+  }
+  activePlayerNameElement.textContent = players[activePlayer].name;
+  gameAreaElement.style.display = "block";
 }
 
 function switchPlayer() {
-    if (activePlayer === 0) {
-        activePlayer = 1;
-    } else {
-        activePlayer = 0;
-    }
-    activePlayerNameElement.textContent = players[activePlayer].name;
+  if (activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+  activePlayerNameElement.textContent = players[activePlayer].name;
 }
 
 function selectGameField(event) {
-    
-    event.target.textContent = players[activePlayer].symbol;
-    event.target.classList.add('disabled');
+  const selectedField = event.target;
+  const selectedColumn = +selectedField.dataset.col - 1;
+  const selectedRow = +selectedField.dataset.row - 1;
 
-    
-    switchPlayer();
-    
+  if (gameData[selectedRow][selectedColumn] > 0) {
+    return;
+  }
+
+  selectedField.textContent = players[activePlayer].symbol;
+  selectedField.classList.add("disabled");
+
+  gameData[selectedRow][selectedColumn] = activePlayer + 1;
+  console.log(gameData);
+
+  switchPlayer();
 }
